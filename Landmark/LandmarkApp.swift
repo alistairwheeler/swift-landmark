b8f1a3aef5035475d15941f0e624fe9e32751609
awheeler <awheeler@simplicite.fr>
2021-12-30 15:36:19 +0100

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Alistair Wheeler on 30/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
