93dd77a046c2727d55600e01102cad6de5f62e2b
awheeler <awheeler@simplicite.fr>
2021-12-30 18:09:56 +0100

    Login page

--- Landmark/ContentView.swift
+++ Landmark/ContentView.swift
@@ -7,15 +7,58 @@
 
 import SwiftUI
 
+let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
+
 struct ContentView: View {
+    @State var username: String = ""
+    @State var password: String = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            WelcomeText()
+            WelcomeImage()
+            TextField("Username", text:$username)
+                .padding()
+                .background(lightGreyColor)
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
+            SecureField("Password", text:$password)  .padding()
+                .background(lightGreyColor)
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
+            Text("LOGIN")
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .frame(width: 220, height: 60)
+                .background(Color.green)
+                .cornerRadius(15.0)
+        }.padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WelcomeText: View {
+    var body: some View {
+        Text("Welcome")
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .padding(.bottom, 20)
+    }
+}
+
+struct WelcomeImage: View {
+    var body: some View {
+        Image("userImage")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 150, height: 150)
+            .clipped()
+            .cornerRadius(150)
+            .padding(.bottom, 75)
     }
 }
